c27b8d6491be7def04504e1e96dd8a5bfacaab51
MateoVelasquez <mateo10velasquez@hotmail.com>
2021-06-14 20:51:26 -0500

    Agregado archivo principal

---
+++ Software/hexapod_firmwarecode/hexapod_main.ino
@@ -0,0 +1,11 @@
+/*
+    Nero Hexapod 18DOF
+
+    Version:    1.0
+    Date:       14-06-2021
+    Programmer: Mateo Velásquez  - Main code
+    Licence: CC BY-NC-SA 4.0
+
+    Código para un hexapodo de 18 grados.
+*/
+#include "hexapod_core/Movement.h"
